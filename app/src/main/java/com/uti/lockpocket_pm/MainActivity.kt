91935101bda3b3ab96b245c7c809c097828bd58d
Reva Ekalia <reva_ekalia@teknokrat.ac.id>
2025-06-20 14:17:24 +0700

    commit

--- app/src/main/java/com/uti/lockpocket_pm/MainActivity.kt
+++ app/src/main/java/com/uti/lockpocket_pm/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.uti.lockpocket_pm
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.app.ActivityOptions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button = findViewById<ImageView>(R.id.lg)
+        button.setOnClickListener {
+            val intent = Intent(this, login2Activity::class.java)
+
+            val intent = Intent(this, login2Activity::class.java)
+            val options = ActivityOptions.makeCustomAnimation(this, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            startActivity(intent, options.toBundle())
+
         }
-    }
-}
