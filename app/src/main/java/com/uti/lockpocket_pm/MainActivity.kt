527390a81493e231ea8df5cd6c797e16c324da4d
Reva Ekalia <reva_ekalia@teknokrat.ac.id>
2025-06-20 14:23:58 +0700

    commit

--- app/src/main/java/com/uti/lockpocket_pm/MainActivity.kt
+++ app/src/main/java/com/uti/lockpocket_pm/MainActivity.kt
@@ -2,9 +2,10 @@ package com.uti.lockpocket_pm
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
 import android.app.ActivityOptions
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +15,9 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<ImageView>(R.id.lg)
         button.setOnClickListener {
             val intent = Intent(this, login2Activity::class.java)
-
-            val intent = Intent(this, login2Activity::class.java)
             val options = ActivityOptions.makeCustomAnimation(this, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
             startActivity(intent, options.toBundle())
 
         }
+    }
+}
